ec45bd2ccc2f9c685633a6d553155961fa0ca9df
olga794 <125156680+olgaF794@users.noreply.github.com>
2023-06-04 17:52:26 -0500

    feature text atributos

--- app/src/main/java/com/example/jetpacktutorial2/MainActivity.kt
+++ app/src/main/java/com/example/jetpacktutorial2/MainActivity.kt
@@ -3,62 +3,86 @@ package com.example.jetpacktutorial2
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-//import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-//import androidx.compose.material.MaterialTheme
-//import androidx.compose.material.Surface
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-//import com.example.jetpacktutorial2.ui.theme.JetpackTutorial2Theme
+import androidx.compose.ui.unit.sp
+import com.example.jetpacktutorial2.ui.theme.JetpackTutorial2Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyComponent()
+            JetpackTutorial2Theme() {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    MyText()
+                }
+            }
+
         }
     }
 }
 
+
+
+
+
+@Preview(showBackground = true)
 @Composable
-fun MyComponent(){
-    Row(modifier = Modifier.padding(8.dp)) {
-        MyTexts()
-        Myimage()
+fun DefaultPreview(){
+    JetpackTutorial2Theme {
+        MyText()
     }
 }
 
 @Composable
-fun Myimage(){
-    Image(
-        painterResource(R.drawable.ic_launcher_foreground ),
-        "uso imagen de prueba")
-}
+fun MyText() {
+    Column(Modifier.fillMaxSize()) {
+        Text(text = "EJEmplo")
+        Text(text = "EJEmplo 2 color", color = Color.Red)
+        Text(text = "EJEmplo 3 Fuente 1 ", fontWeight = FontWeight.SemiBold)
+        Text(text = "EJEmplo 4 fuente 2", fontWeight = FontWeight.ExtraBold)
+        Text(
+            text = "EJEmplo 5 estilo tipo fuente ",
+            style = TextStyle(fontFamily = FontFamily.Cursive)
+        )
+        Text(
+            text = "EJEmplo 5 estilo decoracion 1",
+            style = TextStyle(textDecoration = TextDecoration.Underline)
+        )
+        Text(
+            text = "EJEmplo 5 estilo decoracion 2 ",
+            style = TextStyle(textDecoration = TextDecoration.LineThrough)
+        )
+        Text(
+            text = "EJEmplo 5 estilo decoracion 2 y 1 ", style = TextStyle(
+                textDecoration = TextDecoration.combine(
+                    listOf(TextDecoration.LineThrough, TextDecoration.Underline)
+                )
+            )
+        )
+        // se puede usar asi
+        Text(text = "EJEmplo 5 estilo tipo fuente  sin usar style", fontFamily = FontFamily.Cursive)
+        Text(text = "EJEmplo 5 tamaño de texto", fontSize = 20.sp)
 
 
-@Composable
-fun MyTexts() {
-    Column() {
-        MyText("hola mundo")
-        MyText("listo")
+
+
     }
-
-}
-
-@Composable
-fun MyText(text: String) {
-    Text(text)
-}
-@Preview
-@Composable
-fun PreviewComponet(){
-    MyComponent()
 
 }
