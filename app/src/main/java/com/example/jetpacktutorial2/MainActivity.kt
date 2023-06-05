d084b0b4d348e96d720a1779f4b38158c8854ba9
olga794 <125156680+olgaF794@users.noreply.github.com>
2023-06-04 20:46:17 -0500

    feature text avanzados

--- app/src/main/java/com/example/jetpacktutorial2/MainActivity.kt
+++ app/src/main/java/com/example/jetpacktutorial2/MainActivity.kt
@@ -5,10 +5,13 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
@@ -30,59 +33,100 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    MyText()
-                }
-            }
+                    //MyTextField()
+                    Column() {
+                        MyTextFieldAdVance()
+                        MyTextFieldOutLined()
+                    }
 
+                }
+
+            }
         }
     }
-}
-
-
-
 
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview(){
+fun DefaultPreview() {
     JetpackTutorial2Theme {
-        MyText()
+        //MyTextField()
+        MyTextFieldAdVance()
+        MyTextFieldOutLined()
     }
+}
+
+    @Composable
+    fun MyTextFieldOutLined() {
+        var myTextVaria by remember { mutableStateOf("") }
+        OutlinedTextField(
+            value = myTextVaria,
+            onValueChange = { myTextVaria = it },
+            modifier = Modifier.padding(24.dp),
+            label = {Text(text = "text_line")},
+            colors = TextFieldDefaults.outlinedTextFieldColors(
+                focusedBorderColor = Color.Cyan,
+                unfocusedBorderColor = Color.Yellow
+            )
+        )
+
+    }
+
+    @Composable
+    fun MyTextFieldAdVance() {
+        var myTextVariaA by remember { mutableStateOf("") }
+        //TextField(value = myTextVariaA, onValueChange = { myTextVariaA = it }, label = {Text(text = "Escribe Nombre")})
+        // agregando comprobaciones
+        TextField(value = myTextVariaA, onValueChange = {
+            myTextVariaA = if (it.contains("a")) {
+                it.replace("a","rr")
+            } else {
+                it
+            }
+        }, label = { Text(text = "Escribe Nombre") })
+
+
+    }
+@Composable
+fun MyTextField() {
+    var myTextVaria by remember { mutableStateOf("olga") }
+    TextField(value = myTextVaria, onValueChange = { myTextVaria = it })
+
+
 }
 
 @Composable
 fun MyText() {
     Column(Modifier.fillMaxSize()) {
-        Text(text = "EJEmplo")
-        Text(text = "EJEmplo 2 color", color = Color.Red)
-        Text(text = "EJEmplo 3 Fuente 1 ", fontWeight = FontWeight.SemiBold)
-        Text(text = "EJEmplo 4 fuente 2", fontWeight = FontWeight.ExtraBold)
-        Text(
-            text = "EJEmplo 5 estilo tipo fuente ",
-            style = TextStyle(fontFamily = FontFamily.Cursive)
-        )
-        Text(
-            text = "EJEmplo 5 estilo decoracion 1",
-            style = TextStyle(textDecoration = TextDecoration.Underline)
-        )
-        Text(
-            text = "EJEmplo 5 estilo decoracion 2 ",
-            style = TextStyle(textDecoration = TextDecoration.LineThrough)
-        )
-        Text(
-            text = "EJEmplo 5 estilo decoracion 2 y 1 ", style = TextStyle(
-                textDecoration = TextDecoration.combine(
-                    listOf(TextDecoration.LineThrough, TextDecoration.Underline)
-                )
-            )
-        )
-        // se puede usar asi
-        Text(text = "EJEmplo 5 estilo tipo fuente  sin usar style", fontFamily = FontFamily.Cursive)
-        Text(text = "EJEmplo 5 tamaño de texto", fontSize = 20.sp)
-
-
-
 
     }
+    Text(text = "EJEmplo")
+    Text(text = "EJEmplo 2 color", color = Color.Red)
+    Text(text = "EJEmplo 3 Fuente 1 ", fontWeight = FontWeight.SemiBold)
+    Text(text = "EJEmplo 4 fuente 2", fontWeight = FontWeight.ExtraBold)
+    Text(
+        text = "EJEmplo 5 estilo tipo fuente ",
+        style = TextStyle(fontFamily = FontFamily.Cursive)
+    )
+    Text(
+        text = "EJEmplo 5 estilo decoracion 1",
+        style = TextStyle(textDecoration = TextDecoration.Underline)
+    )
+    Text(
+        text = "EJEmplo 5 estilo decoracion 2 ",
+        style = TextStyle(textDecoration = TextDecoration.LineThrough)
+    )
+    Text(
+        text = "EJEmplo 5 estilo decoracion 2 y 1 ", style = TextStyle(
+            textDecoration = TextDecoration.combine(
+                listOf(TextDecoration.LineThrough, TextDecoration.Underline)
+            )
+        )
+    )
+    // se puede usar asi
+    Text(text = "EJEmplo 5 estilo tipo fuente  sin usar style", fontFamily = FontFamily.Cursive)
+    Text(text = "EJEmplo 5 tamaño de texto", fontSize = 20.sp)
 
+
+}
 }
